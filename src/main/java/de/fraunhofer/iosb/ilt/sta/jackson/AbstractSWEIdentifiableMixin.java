fdb14759b8e7f7ea493031b58064ed097c154e1a
Jacoby <michael.jacoby@iosb.fraunhofer.de>
2019-11-07 15:27:53 +0100

    refactoring de-/serialization for tasking

--- src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/AbstractSWEIdentifiableMixin.java
+++ src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/AbstractSWEIdentifiableMixin.java
@@ -1,25 +1,7 @@
-/*
- * Copyright (C) 2016 Fraunhofer Institut IOSB, Fraunhoferstr. 1, D 76131
- * Karlsruhe, Germany.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package de.fraunhofer.iosb.ilt.sta.jackson;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
-import de.fraunhofer.iosb.ilt.sta.jackson.customtyping.ConfigurableClassAnnotationTypeIdResolver;
 
 /**
  *
@@ -27,6 +9,6 @@ import de.fraunhofer.iosb.ilt.sta.jackson.customtyping.ConfigurableClassAnnotati
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CUSTOM, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonTypeIdResolver(ConfigurableClassAnnotationTypeIdResolver.class)
-public abstract class AbstractSWEIdentifiableMixin {   
+public abstract class AbstractSWEIdentifiableMixin {
 
 }
--- src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/ConfigurableClassAnnotationTypeIdResolver.java
+++ src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/ConfigurableClassAnnotationTypeIdResolver.java
@@ -1,21 +1,4 @@
-/*
- * Copyright (C) 2016 Fraunhofer Institut IOSB, Fraunhoferstr. 1, D 76131
- * Karlsruhe, Germany.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package de.fraunhofer.iosb.ilt.sta.jackson.customtyping;
+package de.fraunhofer.iosb.ilt.sta.jackson;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.DatabindContext;
@@ -23,10 +6,7 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.jsontype.TypeIdResolver;
 import de.fraunhofer.iosb.ilt.configurable.annotations.ConfigurableClass;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import org.reflections.Reflections;
 
@@ -37,11 +17,10 @@ import org.reflections.Reflections;
 public class ConfigurableClassAnnotationTypeIdResolver implements TypeIdResolver {
 
     private JavaType superType;
-    private static String foo;
     private static final Map<String, Class<?>> annnotatedClasses = new Reflections("de.fraunhofer.iosb.ilt.swe.common")
             .getTypesAnnotatedWith(ConfigurableClass.class, true).stream()
             .filter(x -> !x.getAnnotation(ConfigurableClass.class).jsonName().isEmpty())
-            .collect(Collectors.toMap(x -> x.getAnnotation(ConfigurableClass.class).jsonName(), x -> x));  
+            .collect(Collectors.toMap(x -> x.getAnnotation(ConfigurableClass.class).jsonName(), x -> x));
 
     @Override
     public void init(JavaType baseType) {
